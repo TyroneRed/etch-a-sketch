a19c43f854525de369d39f7ca9bc88641cb2d52e
Maureen Hyde <maureenhyde@gmail.com>
2024-04-04 12:19:09 -0400

    change container and mouseover for mobile

---
+++ jsformobile.js
@@ -0,0 +1,60 @@
+
+let containerSize = 500;
+let numPerSide = 10;
+
+function createColumns() {
+// create columns
+    for(let i=0; i<numPerSide; i++){
+        container = document.querySelector('#container');
+        column = document.createElement('div');
+        column.classList.add("column", "grid");
+        container.appendChild(column);
+};
+};
+function createCells() {
+// create cells
+columns = document.querySelectorAll(".column");
+columns.forEach((column) => {
+    for(let j=0; j<numPerSide; j++){
+        cell=document.createElement("div");
+        cell.classList.add("cell", "grid");
+        cell.style.height = containerSize/numPerSide - 2 + "px";
+        column.appendChild(cell);
+    }
+});
+}
+
+function addClickEffect() {
+// add mouseover listener to each cell to colorize background
+cells = document.querySelectorAll('.cell');
+cells.forEach((cell) => {
+    cell.addEventListener("click", function colorCell(){
+        r = Math.floor(Math.random() * 256);
+        g = Math.floor(Math.random() * 256);
+        b = Math.floor(Math.random() * 256);
+        cell.style.backgroundColor = `rgb(${r},${g},${b})`;
+        //alternatively
+        //cell.style.backgroundColor = "blue"; for solid colorizing.
+    });
+});
+};
+
+createColumns();
+createCells();
+addClickEffect();
+
+squaresButton = document.querySelector("Button");
+squaresButton.addEventListener("click", () => {
+    numPerSide = 0;
+    while ((typeof(numPerSide) !== 'number') || (numPerSide < 1) || (numPerSide > 100)){
+    numPerSide = +prompt("Please choose the number of cells per side (1-100):");
+    numPerSide = Math.round(numPerSide);
+}
+ 
+    gridElements = document.querySelectorAll('.grid');
+    gridElements.forEach(gridElement => {gridElement.remove()});
+    createColumns();
+    createCells();
+    addClickEffect();
+    }
+);  
