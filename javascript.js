7f375e39f3da226d8e4bb22f28c8c12c6a683803
Maureen Hyde <maureenhyde@gmail.com>
2024-04-04 08:35:48 -0400

    add logic to remove grid elements on button click

--- javascript.js
+++ javascript.js
@@ -1,12 +1,15 @@
 
-let numPerSide = prompt('How many squares per side of the grid do you want? ');
 let containerSize = 960;
+let numPerSide = 16;
+
+
+
 
 // create columns
 for(let i=0; i<numPerSide; i++){
     container = document.querySelector('#container');
     column = document.createElement('div');
-    column.classList = "column";
+    column.classList.add("column", "grid");
     container.appendChild(column);
 };
 // create cells
@@ -14,7 +17,7 @@ columns = document.querySelectorAll(".column");
 columns.forEach((column) => {
     for(let j=0; j<numPerSide; j++){
         cell=document.createElement("div");
-        cell.classList = "cell";
+        cell.classList.add("cell", "grid");
         cell.style.height = containerSize/numPerSide + "px";
         column.appendChild(cell);
     }
@@ -27,5 +30,12 @@ cells.forEach((cell) => {
         cell.style.backgroundColor = "blue";
     });
 });
+
+squaresButton = document.querySelector("Button");
+squaresButton.addEventListener("click", () => {
+    numPerSide = prompt('How many squares per side of the grid do you want? ');
+    gridElements = document.querySelectorAll('.grid');
+    gridElements.forEach(gridElement => {gridElement.remove()});
+})
 
 
